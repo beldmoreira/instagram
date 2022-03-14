c6d44a16dd1b15c995b9b7ddecd10f7ff31ba887
Isabela <isabela_cmoreira@hotmail.com>
2022-03-13 23:40:24 -0300

    Suggestions component has been refactored using map and props

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,71 +1,37 @@
+function UserSuggestions({image,userName,followingOrNot}){
+  return(
+<div class="sugestao">
+    <div class="usuario">
+      <img src={`assets/img/${image}`}/>
+      <div class="texto">
+        <div class="nome">{userName}</div>
+        <div class="razao">{followingOrNot}</div>
+      </div>
+    </div>
+    <div class="seguir">Seguir</div>
+</div>
+  );
+}
+
+
+
+
 export default function Suggestions(){
+  const suggestions = [
+    {image:"bad.vibes.memes.svg",userName: "bad.vibes.memes",followingOrNot:"Segue você"},
+    {image:"chibirdart.svg",userName: "chibirdart",followingOrNot:"Segue você"},
+    {image:"razoesparaacreditar.svg",userName: "razoesparaacreditar",followingOrNot:"Novo no Instagram"},
+    {image:"adorable_animals.svg",userName: "adorable_animals",followingOrNot:"Segue você"},
+    {image:"smallcutecats.svg",userName: "smallcutecats",followingOrNot:"Segue você"},
+  ]
 
 return(
 <div class="sugestoes">
   <div class="titulo">
     Sugestões para você
     <div>Ver tudo</div>
-  </div>
-
-  <div class="sugestao">
-    <div class="usuario">
-      <img src="assets/img/bad.vibes.memes.svg" />
-      <div class="texto">
-        <div class="nome">bad.vibes.memes</div>
-        <div class="razao">Segue você</div>
-      </div>
-    </div>
-
-    <div class="seguir">Seguir</div>
-  </div>
-
-  <div class="sugestao">
-    <div class="usuario">
-      <img src="assets/img/chibirdart.svg" />
-      <div class="texto">
-        <div class="nome">chibirdart</div>
-        <div class="razao">Segue você</div>
-      </div>
-    </div>
-
-    <div class="seguir">Seguir</div>
-  </div>
-
-  <div class="sugestao">
-    <div class="usuario">
-      <img src="assets/img/razoesparaacreditar.svg" />
-      <div class="texto">
-        <div class="nome">razoesparaacreditar</div>
-        <div class="razao">Novo no Instagram</div>
-      </div>
-    </div>
-
-    <div class="seguir">Seguir</div>
-  </div>
-
-  <div class="sugestao">
-    <div class="usuario">
-      <img src="assets/img/adorable_animals.svg" />
-      <div class="texto">
-        <div class="nome">adorable_animals</div>
-        <div class="razao">Segue você</div>
-      </div>
-    </div>
-
-    <div class="seguir">Seguir</div>
-  </div>
-
-  <div class="sugestao">
-    <div class="usuario">
-      <img src="assets/img/smallcutecats.svg" />
-      <div class="texto">
-        <div class="nome">smallcutecats</div>
-        <div class="razao">Segue você</div>
-      </div>
-    </div>
-
-    <div class="seguir">Seguir</div>
-  </div>
+  </div> 
+  {suggestions.map((suggestion) => <UserSuggestions image ={suggestion.image} userName = {suggestion.userName} followingOrNot = {suggestion.followingOrNot}/>)}
 </div>
 );
 }
